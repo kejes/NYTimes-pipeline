b29d7208f062656332ce3500d72696b7b4cf0989
p.m.m.cox@tilburguniversity.edu <Paul@Pauls-MacBook-Pro.local>
2017-12-28 16:28:08 +0100

    Added dynamic API key read code

--- ingestion_to_kafka.py
+++ ingestion_to_kafka.py
@@ -7,12 +7,18 @@ file_path = "/home/u220850/news_flash_pipeline/"
 
 print(dt.datetime.now().strftime(format='%c'))
 
+
 def decode_http_response(r):
     assert r.status == 200, 'HTTP request did not return a valid status code'
     return r.data.decode()
 
+
 http = urllib3.PoolManager()
-api_key = "8e1ff87091aa47dc9b35742e1445065d" # Add own API key
+
+# Add own API key
+with open("NYTimes-pipeline/api_key.txt", 'r') as f:
+    api_key = f.read()
+
 meta_data_url = "http://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=" + api_key
 meta_data_url_offset = url = "http://api.nytimes.com/svc/news/v3/content/all/all.json?" + urllib3.request.urlencode({
     'api-key': api_key,
@@ -23,15 +29,15 @@ r2 = http.request('GET', url=meta_data_url_offset)  # Fetch 20 new articles
 
 all_recent_articles = json.loads(decode_http_response(r))  # Type = dictionary
 all_recent_articles_offset = json.loads(decode_http_response(r2))  # Type = dict
-combined = all_recent_articles['results'] + all_recent_articles_offset['results'] # Type array of JSON objects
+combined = all_recent_articles['results'] + \
+    all_recent_articles_offset['results']  # Type array of JSON objects
 
 print("Results fetched")
 
-client = KafkaClient(hosts="127.0.0.1:9092") # Default port 9092
-topic = client.topics[b'kafka_nyt'] # select or create new topic
+client = KafkaClient(hosts="127.0.0.1:9092")  # Default port 9092
+topic = client.topics[b'kafka_nyt']  # select or create new topic
 
 with topic.get_sync_producer() as producer:
-    producer.produce(str.encode(json.dumps(combined))) # Write array of JSON file
+    producer.produce(str.encode(json.dumps(combined)))  # Write array of JSON file
 
 print("Files written...done")
-
